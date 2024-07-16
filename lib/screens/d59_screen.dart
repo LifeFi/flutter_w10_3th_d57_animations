7707084a3842aa09973af404186c91faf43d96b8
topcircle <topcircle@gmail.com>
2024-07-16 18:24:38 +0900

    d59

--- lib/screens/d59_screen.dart
+++ lib/screens/d59_screen.dart
@@ -7,16 +7,106 @@ class D59Screen extends StatefulWidget {
   State<D59Screen> createState() => _D59ScreenState();
 }
 
-class _D59ScreenState extends State<D59Screen> {
+class _D59ScreenState extends State<D59Screen>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(milliseconds: 1500),
+    vsync: this,
+  )..repeat();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
         title: const Text("D59"),
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.white,
       ),
-      body: const Center(
-        child: Text("D59"),
-      ),
+      body: Center(
+          child: Container(
+        width: 350,
+        height: 350,
+        padding: const EdgeInsets.all(30),
+        color: Colors.black,
+        child: GridView.builder(
+          itemCount: 25,
+          reverse: true,
+          padding: EdgeInsets.zero,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 5,
+          ),
+          itemBuilder: (context, index) {
+            final row = index ~/ 5;
+            if (row % 2 == 0) {
+              index = row * 5 + 4 - (index % 5);
+            }
+            final fadeInOutTween = TweenSequence([
+              TweenSequenceItem(
+                tween: Tween(begin: 0.0, end: 1.0).chain(
+                  CurveTween(
+                    curve: Curves.bounceIn,
+                  ),
+                ),
+                weight: 4,
+              ),
+              TweenSequenceItem(
+                tween: Tween(begin: 1.0, end: 1.0),
+                weight: 1,
+              ),
+              TweenSequenceItem(
+                tween: Tween(begin: 1.0, end: 0.0).chain(
+                  CurveTween(
+                    curve: Curves.easeInExpo,
+                  ),
+                ),
+                weight: 5,
+              ),
+            ]).animate(
+              CurvedAnimation(
+                parent: _controller,
+                curve: Interval(
+                  (index / 50),
+                  ((index + 25) / 50),
+                ),
+              ),
+            );
+
+            return ScaleTransition(
+              scale: fadeInOutTween,
+              child: FadeTransition(
+                opacity: fadeInOutTween,
+                child: Container(
+                  margin: const EdgeInsets.all(15),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(3),
+                  ),
+                  // child: Text(
+                  //   index.toString(),
+                  //   style: const TextStyle(
+                  //     color: Colors.white,
+                  //     fontSize: 10,
+                  //   ),
+                  // ),
+                ),
+              ),
+            );
+          },
+        ),
+      )),
     );
   }
 }
